3f6686e382c6d2fd699b0d0a341b7f65c64d973c
Xenos F <git@xenosfio.com>
2022-06-14 05:44:33 +0800

    add top bar

---
+++ components/TopBar/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import styles from "../../styles/TopBar.module.css";
+
+const TopBar = (props: { homeButton?: boolean }) => {
+  return (
+    <div className={styles.topBar}>
+      <span className={styles.topBarTitle}>SEBadge</span>
+      {props.homeButton && (
+        <a href="/verify" className={styles.topBarButton}>
+          Verify another badge
+        </a>
+      )}
+    </div>
+  );
+};
+
+export default TopBar;
