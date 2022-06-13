08701bbedff559da36b8d54cae35c21b1c29b9b5
Xenos F <git@xenosfio.com>
2022-06-14 03:10:19 +0800

    add simple error pages

---
+++ pages/404.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next'
+
+const NotFound: NextPage = () => {
+  return (
+    <p>Not Found</p>
+  )
+  //TODO: make nicer
+}
+
+export default NotFound
---
+++ pages/500.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next'
+
+const ServerError: NextPage = () => {
+  return (
+    <p>Internal Server Error</p>
+  )
+  //TODO: make nicer
+}
+
+export default ServerError
