c18e43fee7d8c2826cea79658143a68d8c7f4ee1
Xenos F <git@xenosfio.com>
2022-06-14 22:02:59 +0800

    move head to app and add meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Verify SEB Badge</title>
+        <meta name="description" content="Web service to verify SEB badges" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=yes, viewport-fit=cover' />
+        <meta name="application-name" content="SEBadge" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="SEBadhe" />
+        <meta name="description" content="Web service to verify SEB badges" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/verify/index.tsx
+++ pages/verify/index.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 import TopBar from "../../components/TopBar";
 import VerifyForm from "../../components/VerifyForm";
 import verifyArt from "../../public/undraw_confirmation_re_b6q5.svg";
@@ -7,14 +6,7 @@ import verifyArt from "../../public/undraw_confirmation_re_b6q5.svg";
 const Verify: NextPage = () => {
   return (
     <>
-      <Head>
-        <title>Verify SEB Badge</title>
-        <meta name="description" content="Web service to verify SEB badges" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <TopBar />
-
       <main>
         <VerifyForm imageSrc={verifyArt} title="Verify badge">
           <p>Enter a badge ID to verify its authenticity, as well as check its details.</p>
