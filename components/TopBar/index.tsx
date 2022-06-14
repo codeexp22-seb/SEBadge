c31e4fb27b21a02530e6b2d634ee072f81624095
Xenos F <git@xenosfio.com>
2022-06-15 00:12:27 +0800

    add icon to top bar

--- components/TopBar/index.tsx
+++ components/TopBar/index.tsx
@@ -1,10 +1,22 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import styles from "./TopBar.module.css";
+import logo from "../../public/SEB_logo.svg";
 
 const TopBar = (props: { homeButton?: boolean }) => {
   return (
     <div className={styles.topBar}>
+      <div className={styles.topBarIcon}>
+        <Image
+          src={logo}
+          alt="SEB logo"
+          width="100%"
+          height="100%"
+          layout="responsive"
+          objectFit="contain"
+        ></Image>
+      </div>
       <div className={styles.topBarTitle}>
         <Link href="/">
           <a className={styles.topBarTitleLink}>SEBadge</a>
