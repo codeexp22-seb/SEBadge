1ffde773ad861f8d62487ee9a0cb790450a09db9
Thinker Pal <thinkerpal@gmail.com>
2022-06-14 16:39:31 +0800

    Made top bar SEBadge clickable and return home

--- components/TopBar/index.tsx
+++ components/TopBar/index.tsx
@@ -5,7 +5,9 @@ import styles from "./TopBar.module.css";
 const TopBar = (props: { homeButton?: boolean }) => {
   return (
     <div className={styles.topBar}>
-      <span className={styles.topBarTitle}>SEBadge</span>
+      <Link href="/verify">
+        <a className={styles.topBarTitle}>SEBadge</a>
+      </Link>
       {props.homeButton && (
         <Link href="/verify">
           <a className={styles.topBarButton}>Verify another badge</a>
